7726a7c3a9f1222047f21794018065233924a8b5
JorgeAlvarez28 <jorge_1322134371@uptecamac.edu.mx>
2025-05-11 22:34:30 -0600

    prueba app_intro

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,4 @@
 extends Control
 
-@onready var btn_start = $VBoxContainer/ButtonStart
-
-func _ready():
-	btn_start.pressed.connect(_on_start_pressed)
-
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+func _on_Timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/start/app_intro.tscn")
